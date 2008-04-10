ecc865e132248701d4fa2ab6ef9d5e0186e0b8ff
james <james@94f099bb-ef87-469a-bf14-b0303b48b6ab>
2008-04-10 11:45:29 +0000

    Lots of changes, not all of which work in repo yet as needs some tidying.
    git-svn-id: svn+ssh+userv://atreus.tartarus.org/sja/evilpresentation@44 94f099bb-ef87-469a-bf14-b0303b48b6ab

--- Cgi.py
+++ Cgi.py
@@ -6,19 +6,62 @@
 evilpresentations' CGI driver.
 """
 
-import Flickr, cgi, cgitb
+import Flickr, cgi, cgitb, os, time, urllib
 from jinja import Template, Context, FileSystemLoader
 
 class Driver:
     """evilpresentations' CGI driver; initialise with a Flickr object."""
-    def __init__(self, flickr):
+    def __init__(self, flickr, install_dir):
         """Initialise driver."""
         self.flickr = flickr
+        self.install_dir = install_dir
 
-    def do_get(self):
-        """Process a GET request."""
+    def process_request(self):
+        """Process an HTTP request."""
         cgitb.enable()
+        method = os.environ.get('REQUEST_METHOD', 'GET')
 
+        p = os.environ.get('PATH_INFO', '')
+
+        p = p.split('/')
+        p = map(lambda x: urllib.unquote_plus(x), p)
+
+        if method=='GET':
+            self.do_get(p)
+        elif method=='POST':
+            self.do_post(p)
+        else:
+            print "Status: 405 No, that ain't gonna work"
+            # FIXME: should detect if POST is allowed...
+            print "Allow: GET"
+            print
+
+    def do_post(self, p):
+        """Process a POST request."""
+
+        if p[1]=='presentation':
+            self.make_presentation(p[2:])
+        else:
+            print "Status: 405 Never with POST, pretty boy"
+            print "Allow: GET"
+            print
+
+    def do_get(self, p):
+        """Process a GET request."""
+
+        if p[1]=='presentation':
+            self.do_presentation(p[2:])
+        else:
+            self.do_index()
+
+    def do_index(self):
+        tmpl = Template('index', FileSystemLoader(self.install_dir))
+        c = Context()
+        print "Content-Type: text/html; charset=utf-8"
+        print
+        print tmpl.render(c)
+
+    def _get_photos(self, p):
         start = self.flickr.get_photos('start')
         middle = self.flickr.get_photos('middle', 8)
         end = self.flickr.get_photos('end')
@@ -28,6 +71,49 @@ class Driver:
         photos.extend(middle)
         photos.extend(end)
 
-        tmpl = Template('presentation', FileSystemLoader('.'))
-        c = Context({'photos':photos})
+        return photos
+
+    def _extract_metadata(self, p):
+        affiliation = ''
+
+        try:
+            presenter = p[0]
+            presenter = presenter.split(';')
+            if len(presenter)>1:
+                affiliation = presenter[1]
+            presenter = presenter[0]
+        except IndexError:
+            presenter = ''
+        dateline = time.strftime('%A %B %d, %Y')
+        try:
+            title = p[1]
+        except IndexError:
+            title = dateline
+            dateline = ''
+
+        return {'dateline': dateline, 'title': title, 'presenter': presenter, 'affiliation': affiliation}
+
+    def make_presentation(self, p):
+        photos = self._get_photos(p)
+        metadata = self._extract_metadata(p)
+        metadata['photos'] = photos
+
+        print "Status: 201 Yeah, I've done it already"
+        print "Location: " + self._make_uri(metadata)
+        print
+
+    def _make_uri(self, metadata):
+        # FIXME
+        return '/wibble'
+
+    def do_presentation(self, p):
+        # FIXME: cope with whatever _make_uri returns
+        photos = self._get_photos(p)
+        metadata = self._extract_metadata(p)
+        metadata['photos'] = photos
+
+        tmpl = Template('presentation', FileSystemLoader(self.install_dir))
+        c = Context(metadata)
+        print "Content-Type: text/html; charset=utf-8"
+        print
         print tmpl.render(c)
--- present.py
+++ present.py
@@ -4,13 +4,15 @@
 #
 # Copyright (C) 2008 James Aylett
 
+install_dir = '/home/james/projects/sja/evilpresentation'
+
 import sys
-sys.path.append('/home/james/projects/sja/evilpresentation')
+sys.path.append(install_dir)
 
 import Flickr, Cgi
 
 api_key = '724e7d87e4cda750215d7c9e192adee3'
 
 f = Flickr.Flickr(api_key)
-c = Cgi.Driver(f)
+c = Cgi.Driver(f, install_dir)
 c.do_get()
